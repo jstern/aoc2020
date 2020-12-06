2f0bb5f2646a7f68fe8e1c6aeba3c8c87166b06c
Jon Stern <jonathan.a.stern@gmail.com>
2020-12-06 16:11:35 -0600

    day1 part 2

--- src/aoc2020/day1.clj
+++ src/aoc2020/day1.clj
@@ -1,14 +1,45 @@
 (ns aoc2020.day1
   (:require [aoc2020.util :as util]))
 
-(defn find-value
+(defn int-set
+  []
+  (->> (util/input "day1.txt")
+       (clojure.string/split-lines)
+       (map #(Integer/parseInt %))
+       (set)))
+
+(defn find-values
+  "return the vec [v y] such that y = n - v and y is in the set of values"
   [n values]
-  ;; return the value such that n - value is in the set of values
-  (first (filter #(not (nil? (get values (- n %)))) values)))
+  (let [v (first (filter #(not (nil? (get values (- n %)))) values))
+        y (if (nil? v) nil (- n v))]
+    (cond
+      (nil? v) nil  ;; we didn't find our values
+      (= v y)  nil  ;; we only found v | 2v = n
+      :else    [v y])))
 
 (defn part1
   []
-  (let [input  (util/input "day1.txt")
-        values (set (map #(Integer/parseInt %) (clojure.string/split-lines input)))
-        value  (find-value 2020 values)]
-    (* value (- 2020 value))))
+  (->> (int-set)
+       (find-values 2020)
+       (reduce *)))
+
+(defn sum-pairs
+  "map of x + y to #{x y}"
+  [xs]
+  (into {}
+        (for [x1 xs
+              x2 xs
+              :when (not (= x1 x2))]
+          [(+ x1 x2) #{x1 x2}])))
+
+(defn part2
+  "Get a map of x+y to x, y values
+   Find 2020 - (x+y) in the original set of values
+   Use that to look up x and y, return x * y * (2020 - (x+y))"
+  []
+  (let [zs   (int-set)
+        xys  (sum-pairs zs)
+        x+y  (first (filter #(contains? zs (- 2020 %)) (keys xys)))
+        xy   (get xys x+y)]
+    (* (reduce * xy) (- 2020 x+y))))
--- test/aoc2020/day1_test.clj
+++ test/aoc2020/day1_test.clj
@@ -2,8 +2,19 @@
   (:require [clojure.test :refer :all]
             [aoc2020.day1 :refer :all]))
 
-(deftest test-find-value
-  (testing "finds a value that sums to n with another value"
-    ;; guess it can find them in either order...
-    (is (or (= 2 (find-value 5 (set [1 2 3])))
-            (= 3 (find-value 5 (set [1 2 3])))))))
+(deftest test-find-values
+  (testing "finds 2 values that sum to n with another value"
+    (is (= (set (find-values 5 (set [1 2 3])))
+           (set [2 3]))))
+  (testing "returns nil when n - v is not in the set"
+    (is (nil? (find-values 7 (set [1 2 3])))))
+  (testing "returns nil when n - v = v (not enough values)"
+    (is (nil? (find-values 6 (set [3])))))
+  (testing "finds valid pair even if invalid pair exists"
+    (is (= (set (find-values 4 (set [1 2 3])))
+           (set [1 3])))))
+
+(deftest test-pairs
+  (testing "returns {(x1 + x2) #{x1 x2}, ...}"
+    (is (= {3 #{1 2} 4 #{1 3} 5 #{2 3}}
+           (sum-pairs [1 2 3])))))
