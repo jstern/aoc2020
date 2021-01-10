309461e8ade7d2bed96dddb22d5a2568f4214bc0
Jon Stern <jonathan.a.stern@gmail.com>
2021-01-09 18:51:36 -0600

    day12 part2

--- src/aoc2020/day12.clj
+++ src/aoc2020/day12.clj
@@ -13,12 +13,16 @@
 
 (def turndirs {"L" -1 "R" 1})
 
-(def ship {:heading "E" "N" 0 "E" 0 "S" 0 "W" 0})
+(def ship1 {:heading "E" "N" 0 "E" 0 "S" 0 "W" 0})
 
 (defn h2d [h] (get {"N" 0 "E" 90 "S" 180 "W" 270} h))
 (defn d2h [d] (get {0 "N" 90 "E" 180 "S" 270 "W"} d))
 
-(defn turn
+(defn parse
+  [instruction]
+  [(subs instruction 0 1) (Integer/valueOf (subs instruction 1))])
+
+(defn turn1
   [heading direction amount]
   (let [deg (h2d heading)]
     (-> (get turndirs direction)
@@ -29,11 +33,9 @@
 
 (defn nav1
   [ship instruction]
-  (let [action (subs instruction 0 1)
-        amount (Integer/valueOf (subs instruction 1))]
-    (println ship instruction)
+  (let [[action amount] (parse instruction)]
     (condp clojure.string/includes? action
-      "LR"   (update ship :heading turn action amount)
+      "LR"   (update ship :heading turn1 action amount)
       "NSEW" (update ship action + amount)
       "F"    (update ship (:heading ship) + amount))))
 
@@ -41,7 +43,58 @@
   []
   (let [end (->> (util/input "day12.txt")
                  (clojure.string/split-lines)
-                 (reduce nav1 ship))]
+                 (reduce nav1 ship1))]
     (+ (Math/abs (- (get end "N") (get end "S")))
        (Math/abs (- (get end "E") (get end "W"))))))
-(defn part2 [] "")
+
+(comment
+    Action N means to move the waypoint north by the given value.
+    Action S means to move the waypoint south by the given value.
+    Action E means to move the waypoint east by the given value.
+    Action W means to move the waypoint west by the given value.
+    Action L means to rotate the waypoint around the ship left (counter-clockwise) the given number of degrees.
+    Action R means to rotate the waypoint around the ship right (clockwise) the given number of degrees.
+    Action F means to move forward to the waypoint a number of times equal to the given value.)
+
+;; this time lets do position [x y] waypoint [x y]
+(def ship2 {:p [0 0] :w [10 1]})
+
+;; 90 degree rotations of waypoint
+(defn rgt [[x y]] [y (* -1 x)])
+(defn lft [[x y]] [(* -1 y) x])
+
+(defn turn2
+  [[dx dy] action amount]
+  (let [f (if (= action "L") lft rgt)
+        n (mod (/ amount 90) 4)]
+    (loop [i 0 r [dx dy]]
+      (if (= i n)
+        r
+        (recur (inc i) (f r))))))
+
+(defn shift2
+  [[dx dy] action amount]
+  (condp = action
+    "N" [dx (+ dy amount)]
+    "S" [dx (- dy amount)]
+    "E" [(+ dx amount) dy]
+    "W" [(- dx amount) dy]))
+
+(defn move2
+  [[x y] [dx dy] amt]
+  [(+ x (* amt dx)) (+ y (* amt dy))])
+
+(defn nav2
+  [ship instruction]
+  (let [[action amount] (parse instruction)]
+    (condp clojure.string/includes? action
+      "LR"   (update ship :w turn2 action amount)
+      "NSEW" (update ship :w shift2 action amount)
+      "F"    (update ship :p move2 (:w ship) amount))))
+
+(defn part2
+  []
+  (let [end (->> (util/input "day12.txt")
+                 (clojure.string/split-lines)
+                 (reduce nav2 ship2))]
+    (reduce + (map #(Math/abs %) (:p end)))))
--- test/aoc2020/day12_test.clj
+++ test/aoc2020/day12_test.clj
@@ -2,37 +2,66 @@
   (:require [clojure.test :refer :all]
             [aoc2020.day12 :as d12]))
 
-(deftest test-turn
+(deftest test-turn1
   (testing "R"
-    (is (= "S" (d12/turn "E" "R" 90)))
-    (is (= "W" (d12/turn "E" "R" 180)))
-    (is (= "N" (d12/turn "E" "R" 270)))
-    (is (= "E" (d12/turn "E" "R" 360))))
+    (is (= "S" (d12/turn1 "E" "R" 90)))
+    (is (= "W" (d12/turn1 "E" "R" 180)))
+    (is (= "N" (d12/turn1 "E" "R" 270)))
+    (is (= "E" (d12/turn1 "E" "R" 360))))
   (testing "L"
-    (is (= "N" (d12/turn "E" "L" 90)))
-    (is (= "W" (d12/turn "E" "L" 180)))
-    (is (= "S" (d12/turn "E" "L" 270)))
-    (is (= "E" (d12/turn "E" "L" 360)))))
+    (is (= "N" (d12/turn1 "E" "L" 90)))
+    (is (= "W" (d12/turn1 "E" "L" 180)))
+    (is (= "S" (d12/turn1 "E" "L" 270)))
+    (is (= "E" (d12/turn1 "E" "L" 360)))))
 
 (deftest test-nav1
   (testing "R"
     (is (= {:heading "S" "N" 0 "E" 0 "S" 0 "W" 0}
-           (d12/nav1 d12/ship "R90"))))
+           (d12/nav1 d12/ship1 "R90"))))
   (testing "L"
     (is (= {:heading "N" "N" 0 "E" 0 "S" 0 "W" 0}
-           (d12/nav1 d12/ship "L90"))))
+           (d12/nav1 d12/ship1 "L90"))))
   (testing "F"
     (is (= {:heading "E" "N" 0 "E" 10 "S" 0 "W" 0}
-           (d12/nav1 d12/ship "F10"))))
+           (d12/nav1 d12/ship1 "F10"))))
   (testing "N"
     (is (= {:heading "E" "N" 10 "E" 0 "S" 0 "W" 0}
-           (d12/nav1 d12/ship "N10"))))
+           (d12/nav1 d12/ship1 "N10"))))
   (testing "E"
     (is (= {:heading "E" "N" 0 "E" 10 "S" 0 "W" 0}
-           (d12/nav1 d12/ship "E10"))))
+           (d12/nav1 d12/ship1 "E10"))))
   (testing "S"
     (is (= {:heading "E" "N" 0 "E" 0 "S" 10 "W" 0}
-           (d12/nav1 d12/ship "S10"))))
+           (d12/nav1 d12/ship1 "S10"))))
   (testing "W"
     (is (= {:heading "E" "N" 0 "E" 0 "S" 0 "W" 10}
-           (d12/nav1 d12/ship "W10")))))
+           (d12/nav1 d12/ship1 "W10")))))
+
+(deftest test-nav2
+  (testing "F"
+    (is (= {:p [100 10] :w [10 1]}
+           (d12/nav2 d12/ship2 "F10"))))
+  (testing "N"
+    (is (= {:p [0 0] :w [10 3]}
+           (d12/nav2 d12/ship2 "N2"))))
+  (testing "E"
+    (is (= {:p [0 0] :w [12 1]}
+           (d12/nav2 d12/ship2 "E2"))))
+  (testing "S"
+    (is (= {:p [0 0] :w [10 -1]}
+           (d12/nav2 d12/ship2 "S2"))))
+  (testing "W"
+    (is (= {:p [0 0] :w [8 1]}
+           (d12/nav2 d12/ship2 "W2"))))
+  (testing "R0"
+    (is (= {:p [0 0] :w [10 1]}
+           (d12/nav2 d12/ship2 "R0"))))
+  (testing "R90"
+    (is (= {:p [0 0] :w [1 -10]}
+           (d12/nav2 d12/ship2 "R90"))))
+  (testing "R180"
+    (is (= {:p [0 0] :w [-10 -1]}
+           (d12/nav2 d12/ship2 "R180"))))
+  (testing "L90"
+    (is (= {:p [0 0] :w [-1 10]}
+           (d12/nav2 d12/ship2 "L90")))))
