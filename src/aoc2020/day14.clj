38a12921ca499f497314be5808c98ec9736a9511
Jon Stern <jonathan.a.stern@gmail.com>
2021-11-27 17:49:35 -0600

    i don't know what i was doing but i know it doesn't work

--- src/aoc2020/day14.clj
+++ src/aoc2020/day14.clj
@@ -39,4 +39,111 @@
                         (init))]
     (reduce + (vals memory))))
 
-(defn part2 [] "")
+;; part2
+
+(defn nth-bit
+  [addr n]
+  (if (bit-test addr n) \1 \0))
+
+;; 0+? = ?, 1+? = 1, X+? = X
+(defn mask-bit
+  [mbit abit]
+  (if (= mbit \0) abit mbit))
+
+(defn addrs
+  "apply mask to addr to return an 'address space'"
+  [mask addr]
+  (let [m (char-array mask)
+        v (Long/valueOf (re-find #"\d+" addr))
+        c (count m)]
+    (->> (range c)
+         (map #(mask-bit (nth m %) (nth-bit v (- c % 1))))
+         (apply str))))  ;; turn back into string for my poor eyes
+
+(defn disjoint?
+  [b1 b2]
+  (and
+    (not (= b1 b2)) ;; they're different
+    (not (or (= b1 \X) (= b2 \X))))) ;; and neither is X
+
+
+(def subtract-bit
+  ;; what's left if you take away b2 from b1
+  {
+   [\0 \0] \0
+   [\0 \1] \0
+   [\0 \X] \0
+
+   [\1 \0] \1
+   [\1 \1] \0
+   [\1 \X] \0
+
+   [\X \0] \X
+   [\X \1] \0
+   [\X \X] \0
+
+})
+
+(defn minval
+  [addrs]
+  (Long/valueOf (clojure.string/replace addrs #"X" "0") 2))
+
+(defn maxval
+  [addrs]
+  (Long/valueOf (clojure.string/replace addrs #"X" "1") 2))
+
+(defn nth2 [n] (long (Math/pow 2 n)))
+
+(defn subtract
+  "return a version of a1 that does not include any addr in a2"
+  [a1 a2]
+  (let [c (count a1)]
+    
+    (loop [n c acc '()]
+      (if (= n 0)
+        (let [i (- n 1) b1 (nth a1 i) b2 (nth a2 i)]
+          (if (disjoint? b1 b2)
+            (do (println "disjoint!") a1)
+            (recur (dec n) (conj acc (subtract-bit [b1 b2])))))))))
+
+(defn write2
+  [memory addrs v]
+  (let [updated (->> (for [[a av] memory] [(subtract addrs a) av])
+                     (into []))]
+    (conj updated [addrs v])))
+
+
+(defn execute2
+  [[memory mask] instr]
+  (let [[lhs rhs] (clojure.string/split instr #" = ")]
+    (if (= lhs "mask")
+      [memory rhs] ;; save mask and move on
+      (let [masked (addrs mask lhs)]
+        (println masked)
+        (println " " (minval masked))
+        (println " " (maxval masked))
+        [(write2 memory masked (Long/valueOf rhs)) mask]))))
+
+(defn init2
+  [instructions]
+  (let [state [[] nil]]
+    (reduce execute2 state instructions)))
+
+
+;; how many actual bits is a "bit" worth?
+(def bitval {\0 1 \1 1 \X 2 \- 0})
+
+(defn size
+  [bs]
+  (reduce * (map bitval bs)))
+
+(defn part2
+  []
+  (let [[memory _] (->> (util/input "day14.txt")
+                        (clojure.string/split-lines)
+                        (init2))]
+    (println (first memory))
+    (println (first (map #(* (size (first %)) (second %)) memory)))))
+
+;; wrong answer
+;; 2757059467008 too low
--- test/aoc2020/day14_test.clj
+++ test/aoc2020/day14_test.clj
@@ -21,8 +21,56 @@
     (is (= 1 ((d14/make-mask "1") 0)))
     (is (= 1 ((d14/make-mask "1") 1)))))
 
-
 (deftest test-init
   (testing "processes example"
     (let [[mem _] (d14/init example)]
       (is (= {"mem[7]" 101 "mem[8]" 64} mem)))))
+
+(deftest test-addrs
+  (testing "applies mask to addr"
+    (is (= "000000000000000000000000000000X1101X"
+           (d14/addrs "000000000000000000000000000000X1001X" "mem[42]")))
+    (is (= "00000000000000000000000000000001X0XX"
+           (d14/addrs "00000000000000000000000000000000X0XX" "mem[26]")))))
+
+(deftest test-subtract
+  (testing "obvious stuff"
+    (is (= "0" (d14/subtract "0" "1")))
+    (is (= "1" (d14/subtract "1" "0")))
+    (is (= "-" (d14/subtract "0" "0")))
+    (is (= "-" (d14/subtract "X" "X")))
+    (is (= "0" (d14/subtract "X" "1")))
+    (is (= "1" (d14/subtract "X" "0"))))
+  (testing "returns minuend if any bit is disjoint"
+    (is (= "01" (d14/subtract "01" "11"))))
+  (testing "longer spaces"
+    (is (= "01-" (d14/subtract "XXX" "10X")))))
+
+
+(deftest test-size
+  (testing "counts possible addresses in space"
+    (is (= 4 (d14/size "000000000000000000000000000000X1101X"))))
+  (testing "recognizes my - notation"
+    (is (= 0 (d14/size "000000000000000000000000000000X1-01X")))))
+
+
+(def example2
+  [
+   "mask = X1001X"
+   "mem[42] = 100"
+   "mask = 00X0XX"
+   "mem[26] = 1"
+   ])
+
+
+(deftest test-init2
+  (testing "example data"
+    (let [[memory _] (d14/init2 example2)]
+      (println (map #(str % "\n") memory))
+      (is (= 208
+             (reduce
+               +
+               (map #(* (d14/size (first %)) (second %)) memory)))))))
+
+;; [XX0011X0X0010X0X01001X11111011111111 186083]
+;; 23818624
